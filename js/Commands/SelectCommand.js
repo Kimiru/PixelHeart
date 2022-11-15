cf172a17f3e03c42dd6e5459e12047761a204b51
Kimiru <laursim24@gmail.com>
2022-11-15 18:25:56 +0100

    Better selecteion view

--- js/Commands/SelectCommand.js
+++ js/Commands/SelectCommand.js
@@ -89,18 +89,89 @@ export default class SelectCommand extends Command {
         }
     }
 
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx 
+     * @param {PixelImage} image 
+     */
     static drawSelectionRectangle(ctx, image) {
 
         if (this.selectionRectangle) {
 
             ctx.save()
             ctx.scale(1 / image.width, -1 / image.height)
+            ctx.setLineDash([])
             ctx.lineWidth = .2
             ctx.strokeStyle = 'blue'
             ctx.strokeRect(
                 this.selectionRectangle.left - image.width / 2 + .5,
                 this.selectionRectangle.bottom - image.height / 2 + .5,
                 this.selectionRectangle.w, this.selectionRectangle.h)
+
+
+            ctx.lineWidth = .2
+            ctx.setLineDash([1, 1])
+
+
+            ctx.beginPath()
+            ctx.moveTo(-image.width / 2, this.selectionRectangle.bottom - image.height / 2 + .5)
+            ctx.lineTo(this.selectionRectangle.left - image.width / 2 + .5,
+                this.selectionRectangle.bottom - image.height / 2 + .5)
+            ctx.moveTo(-image.width / 2, this.selectionRectangle.bottom - image.height / 2 + .5 + this.selectionRectangle.h)
+            ctx.lineTo(this.selectionRectangle.left - image.width / 2 + .5,
+                this.selectionRectangle.bottom - image.height / 2 + .5 + this.selectionRectangle.h)
+
+            ctx.moveTo(this.selectionRectangle.left - image.width / 2 + .5, -image.height / 2)
+            ctx.lineTo(this.selectionRectangle.left - image.width / 2 + .5,
+                this.selectionRectangle.bottom - image.height / 2 + .5)
+            ctx.moveTo(this.selectionRectangle.left - image.width / 2 + .5 + this.selectionRectangle.w, -image.height / 2)
+            ctx.lineTo(this.selectionRectangle.left - image.width / 2 + .5 + this.selectionRectangle.w,
+                this.selectionRectangle.bottom - image.height / 2 + .5)
+            ctx.stroke()
+
+            ctx.textAlign = 'right'
+            ctx.textBaseline = 'top'
+            ctx.fillStyle = 'lightgray'
+            ctx.font = '1px sans-serif'
+            ctx.fillText(Number(this.selectionRectangle.bottom + .5).toString(),
+                -image.width / 2, this.selectionRectangle.bottom - image.height / 2 + .5)
+            if (this.selectionRectangle.h > 1) {
+                ctx.textBaseline = 'bottom'
+                ctx.fillText(Number(this.selectionRectangle.top - .5).toString(),
+                    -image.width / 2, this.selectionRectangle.bottom - image.height / 2 + .5 + this.selectionRectangle.h)
+            }
+            ctx.textBaseline = 'bottom'
+            ctx.textAlign = 'left'
+            ctx.fillText(Number(this.selectionRectangle.left + .5).toString(),
+                this.selectionRectangle.left - image.width / 2 + .5, -image.height / 2)
+            if (this.selectionRectangle.w > 1) {
+                ctx.textAlign = 'right'
+                ctx.fillText(Number(this.selectionRectangle.right - .5).toString(),
+                    this.selectionRectangle.left - image.width / 2 + .5 + this.selectionRectangle.w, -image.height / 2)
+            }
+
+            if (this.selectionRectangle.h > 3) {
+
+                ctx.textBaseline = 'middle'
+                ctx.textAlign = 'right'
+
+                ctx.fillText(Number(this.selectionRectangle.h).toString(),
+                    -image.width / 2 - 1.5, -image.height / 2 + .5 + this.selectionRectangle.y)
+
+            }
+
+            if (this.selectionRectangle.w > 3) {
+
+                ctx.textBaseline = 'bottom'
+                ctx.textAlign = 'center'
+
+                ctx.fillText(Number(this.selectionRectangle.h).toString(),
+                    -image.width / 2 + .5 + this.selectionRectangle.x, -image.height / 2 - 1.5)
+
+            }
+
+            ctx.setLineDash([])
+
             ctx.restore()
 
         }
